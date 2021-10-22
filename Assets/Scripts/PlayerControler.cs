d9b798d400f1450fd9dd5b6ad6139c4733320b27
ThiagoGarulo <87036238+ThiagoGarulo@users.noreply.github.com>
2021-10-21 22:27:26 -0300

    Reload Script

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -6,19 +6,38 @@ public class PlayerControler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D Player;
     [SerializeField] private float JumpForce;
+
+    private float moveInput;
+    [SerializeField] private float Speed;
+
+
     void Start()
     {
-        Player = GetComponent<Rigidbody2D>();        
+        Player = GetComponent<Rigidbody2D>();    
     }
     void Update()
     {
-        
+        moveInput = Input.GetAxis("Horizontal");
+        Player.velocity = new Vector2(moveInput * Speed, Player.velocity.y);
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform")
         {
             Player.velocity = new Vector2(Player.velocity.x, JumpForce);
+           
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        { Time.timeScale = 0.9f; }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Time.timeScale = 1f;
     }
 }
