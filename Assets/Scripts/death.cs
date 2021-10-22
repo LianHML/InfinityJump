8c3cec55dc58fd8faca73a757036b6f49ee1dcdf
ThiagoGarulo <87036238+ThiagoGarulo@users.noreply.github.com>
2021-10-22 11:51:26 -0300

    Hotfix_ui

--- Assets/Scripts/death.cs
+++ Assets/Scripts/death.cs
@@ -6,12 +6,16 @@ public class death : MonoBehaviour
 {
 
     public GameObject GameOver;
+    public GameObject Move;
+    public GameObject Pause;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             GameOver.SetActive(true);
+            Move.SetActive(false);
+            Pause.SetActive(false);
             Time.timeScale = 0f;
 
         }
